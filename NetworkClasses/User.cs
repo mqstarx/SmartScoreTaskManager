43fe6b3bb6e8bcebba67aca6ce87a19363e8576f
mqstarx <mqstarx@gmail.com>
2018-03-16 15:36:10 +0300

    1

---
+++ NetworkClasses/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkClasses
+{
+    [Serializable]
+    public class User
+    {
+        private int m_Id;
+        private int m_IdParent;
+        private string m_Password;
+        public User() { }
+
+    }
+}
